28b1547e30bb7936bf0cf343a59bc70da2d873ce
Dexametasona <machicadogomezalexander@gmail.com>
2023-01-16 21:54:33 -0500

    creacion de objeto, estilos y animacion final

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,17 +21,133 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    let data;
+    let colorIzq='';
+    let colorDer='';
 
     for (let i=1; i<=150; i++){
       this.BBDD.BD(String(i)).subscribe(x=>{
         try{
-          this.almacen.push(new pokemon(x.name, x.id, x.sprites.front_default, x.types[0].type.name, x.type[1].type.name))
-        }catch{
-          this.almacen.push(new pokemon(x.name, x.id, x.sprites.front_default, x.types[0].type.name, ''))
+          switch(x.types[0].type.name){
+            case 'bug': colorIzq='#9fc139';
+            break;
+            case 'dark': colorIzq='#616371'
+            break
+            case 'dragon': colorIzq='#0873c4'
+            break
+            case 'electric': colorIzq='#f8df66'
+            break
+            case 'fairy': colorIzq='#f29fe7'
+            break
+            case 'fighting': colorIzq='#e2434e'
+            break
+            case 'fire': colorIzq='#fba34a'
+            break
+            case 'flying': colorIzq='#a1bbeb'
+            break
+            case 'ghost': colorIzq='#7372d2'
+            break
+            case 'grass': colorIzq='#5cbf6c'
+            break
+            case 'ground': colorIzq='#d59162'
+            break
+            case 'ice': colorIzq='#85d3cd'
+            break
+            case 'normal': colorIzq='#9ca0a0'
+            break
+            case 'poison': colorIzq='#bb60d6'
+            break
+            case 'psychic': colorIzq='#fd958b'
+            break
+            case 'rock': colorIzq='#d3c58c'
+            break
+            case 'steel': colorIzq='#549aa4'
+            break
+            case 'water': colorIzq='#5ba7e1'
+            break
+          }
+          switch(x.types[1].type.name){
+            case 'bug': colorDer='#9fc139';
+            break;
+            case 'dark': colorDer='#616371'
+            break
+            case 'dragon': colorDer='#0873c4'
+            break
+            case 'electric': colorDer='#f8df66'
+            break
+            case 'fairy': colorDer='#f29fe7'
+            break
+            case 'fighting': colorDer='#e2434e'
+            break
+            case 'fire': colorDer='#fba34a'
+            break
+            case 'flying': colorDer='#a1bbeb'
+            break
+            case 'ghost': colorDer='#7372d2'
+            break
+            case 'grass': colorDer='#5cbf6c'
+            break
+            case 'ground': colorDer='#d59162'
+            break
+            case 'ice': colorDer='#85d3cd'
+            break
+            case 'normal': colorDer='#9ca0a0'
+            break
+            case 'poison': colorDer='#bb60d6'
+            break
+            case 'psychic': colorDer='#fd958b'
+            break
+            case 'rock': colorDer='#d3c58c'
+            break
+            case 'steel': colorDer='#549aa4'
+            break
+            case 'water': colorDer='#5ba7e1'
+            break
+          }
+          this.almacen.push(new pokemon(x.name, x.id, x.sprites.front_default, x.types[0].type.name, x.types[1].type.name, colorIzq, colorDer))
+        }catch (error){
+          switch(x.types[0].type.name){
+            case 'bug': colorIzq='#9fc139';
+            break;
+            case 'dark': colorIzq='#616371'
+            break
+            case 'dragon': colorIzq='#0873c4'
+            break
+            case 'electric': colorIzq='#f8df66'
+            break
+            case 'fairy': colorIzq='#f29fe7'
+            break
+            case 'fighting': colorIzq='#e2434e'
+            break
+            case 'fire': colorIzq='#fba34a'
+            break
+            case 'flying': colorIzq='#a1bbeb'
+            break
+            case 'ghost': colorIzq='#7372d2'
+            break
+            case 'grass': colorIzq='#5cbf6c'
+            break
+            case 'ground': colorIzq='#d59162'
+            break
+            case 'ice': colorIzq='#85d3cd'
+            break
+            case 'normal': colorIzq='#9ca0a0'
+            break
+            case 'poison': colorIzq='#bb60d6'
+            break
+            case 'psychic': colorIzq='#fd958b'
+            break
+            case 'rock': colorIzq='#d3c58c'
+            break
+            case 'steel': colorIzq='#549aa4'
+            break
+            case 'water': colorIzq='#5ba7e1'
+            break
+          }
+          this.almacen.push(new pokemon(x.name, x.id, x.sprites.front_default, x.types[0].type.name, '', colorIzq, colorIzq))
         }
       })
     }
+    console.log(this.almacen)
   }
 
 }
--- src/app/pokemon.module.ts
+++ src/app/pokemon.module.ts
@@ -4,11 +4,15 @@ export class pokemon{
     sprite!:string;
     tipo1!:string;
     tipo2!:string;
-    constructor(name:string, id:number, sprite:string, tipo1:string, tipo2:string){
+    color1!:string;
+    color2!:string;
+    constructor(name:string, id:number, sprite:string, tipo1:string, tipo2:string, color1:string, color2:string){
         this.name=name;
         this.id=id;
         this.sprite=sprite;
         this.tipo1=tipo1;
-        this.tipo2=tipo2
+        this.tipo2=tipo2;
+        this.color1=color1;
+        this.color2=color2
     }
 }
